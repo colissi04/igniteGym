9957031ab2821313479a96e1ae6b8acc4dba1820
colissi04 <rcolissi@gmail.com>
2024-06-12 22:15:54 -0300

    adicionado tab route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { Box, useTheme } from 'native-base'
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes(){
   const { colors } = useTheme();
@@ -11,7 +13,7 @@ export function Routes(){
   return(
     <Box flex={1} bg='gray.700'>
       <NavigationContainer theme={theme} independent>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
